5787b40d5e80f952bd2beb351548cb95b9a68b44
Nguyễn Thành Trung <thanhtrung692k@gmail.com>
2025-03-07 00:40:47 +0700

    fix bug BE

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -371,7 +371,7 @@ SIMPLE_JWT = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
-
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
